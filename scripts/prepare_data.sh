8fb3ee1751eb98caf97821a5456161cc6dea6bbb
JonghwanMun <JonghwanMun@users.noreply.github.com>
2020-08-04 10:55:51 +0900

    Add download link for features used in charades

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -3,4 +3,9 @@
 wget http://cvlab.postech.ac.kr/~jonghwan/data/LGI/LGI_data.tar.gz
 tar zxvf LGI_data.tar.gz
 mv LGI data
-#rm LGI_data.tar.gz
+rm LGI_data.tar.gz
+
+wget http://cvlab.postech.ac.kr/~jonghwan/data/LGI/charades_data.tar.gz
+tar zxvf charades_data.tar.gz
+mv charades data
+rm charades_data.tar.gz
