64ef8df6454d8d48d0e75f1217fdda653ad2ef26
JonghwanMun <JonghwanMun@users.noreply.github.com>
2021-06-28 12:09:37 +0900

    Fix link for downloading data

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-wget http://cvlab.postech.ac.kr/~jonghwan/data/LGI/LGI_data.tar.gz
+wget http://cvlab.postech.ac.kr/research/LGI/LGI_data.tar.gz
 tar zxvf LGI_data.tar.gz
 mv LGI data
 rm LGI_data.tar.gz
 
-wget http://cvlab.postech.ac.kr/~jonghwan/data/LGI/charades_data.tar.gz
+wget http://cvlab.postech.ac.kr/research/LGI/charades_data.tar.gz
 tar zxvf charades_data.tar.gz
 mv charades data
 rm charades_data.tar.gz
