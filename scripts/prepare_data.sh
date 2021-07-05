e6650ddf6187ba0ca8ef76b39650c91cae6708d5
JonghwanMun <JonghwanMun@users.noreply.github.com>
2021-07-05 11:06:16 +0900

    Update prepare_data.sh

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -7,9 +7,9 @@ mv LGI data
 rm LGI_data.tar.gz
 
 ### Download data for activitynet dataset
-wget http://cvlab.postech.ac.kr/~jonghwan/data/LGI/anet_ann_data.tar.gz # annotation + (preprocessed ones)
-mv LGI data
-rm anet_ann_data.tar.gz
+#wget http://cvlab.postech.ac.kr/~jonghwan/data/LGI/anet_ann_data.tar.gz # annotation + (preprocessed ones)
+#mv LGI data
+#rm anet_ann_data.tar.gz
 # please download the C3D features from http://activity-net.org/challenges/2016/download.html
 # move the C3D feature file into data/LGI/feats/
 
